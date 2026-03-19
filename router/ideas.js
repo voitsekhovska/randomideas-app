d3e2429645f88bb4b08705c2476d7890c97a3797
ye.voitsekhovska <sunakojane6@gmail.com>
2026-03-19 21:57:35 +0200

    cleaned up

---
+++ router/ideas.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const router = express.Router();
+
+const ideas = [
+  {
+    id: 1,
+    text: "Practice self-kindness and spend 30 minutes doing something you love today.",
+    tag: "Kindness",
+    username: "JaneEyre",
+    date: "2026-03-19",
+  },
+  {
+    id: 2,
+    text: "Write your partner a list of things you love about them.",
+    tag: "Relationship",
+    username: "JamesJoyce",
+    date: "2023-05-09",
+  },
+  {
+    id: 3,
+    text: "Put your phone away while in the company of others.",
+    tag: "Relationship",
+    username: "VirginiaWolf",
+    date: "2025-09-11",
+  },
+  {
+    id: 4,
+    text: "Purchase extra dog or cat food and bring it to an animal shelter.",
+    tag: "Kindness",
+    username: "JaneEyre",
+    date: "2026-03-19",
+  },
+  {
+    id: 5,
+    text: "Donate old towels or blankets to an animal shelter.",
+    tag: "Recycling",
+    username: "JamesJoyce",
+    date: "2026-01-01",
+  },
+];
+
+// Get all ideas
+router.get("/", (req, res) => {
+  res.json({ success: true, data: ideas });
+});
+
+//Get single idea by id
+router.get("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === parseInt(req.params.id));
+
+  if (!idea) {
+    return res.status(404).json({ success: false, error: "Resource not found" });
+  }
+
+  res.json({ success: true, data: idea });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,61 +3,11 @@ const port = 5000;
 
 const app = express();
 
-const ideas = [
-  {
-    id: 1,
-    text: "Practice self-kindness and spend 30 minutes doing something you love today.",
-    tag: "Kindness",
-    username: "JaneEyre",
-    date: "2026-03-19",
-  },
-  {
-    id: 2,
-    text: "Write your partner a list of things you love about them.",
-    tag: "Relationship",
-    username: "JamesJoyce",
-    date: "2023-05-09",
-  },
-  {
-    id: 3,
-    text: "Put your phone away while in the company of others.",
-    tag: "Relationship",
-    username: "VirginiaWolf",
-    date: "2025-09-11",
-  },
-  {
-    id: 4,
-    text: "Purchase extra dog or cat food and bring it to an animal shelter.",
-    tag: "Kindness",
-    username: "JaneEyre",
-    date: "2026-03-19",
-  },
-  {
-    id: 5,
-    text: "Donate old towels or blankets to an animal shelter.",
-    tag: "Recycling",
-    username: "JamesJoyce",
-    date: "2026-01-01",
-  },
-];
-
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the RandomIdeas API" });
 });
 
-// Get all ideas
-app.get("/api/ideas", (req, res) => {
-  res.json({ success: true, data: ideas });
-});
-
-app.get("/api/ideas/:id", (req, res) => {
-  const idea = ideas.find((idea) => idea.id === parseInt(req.params.id));
-
-  if (!idea) {
-    return res.status(404).json({ success: false, error: "Resource not found" });
-  }
-
-  res.json({ success: true, data: idea });
-});
+const ideasRouter = require("./router/ideas");
+app.use("/api/ideas", ideasRouter)
 
 app.listen(port, () => console.log(`This server listening on post ${port}`));
