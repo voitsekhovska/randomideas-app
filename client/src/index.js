6217363670b697199e174a7e3707d42cf75a23f9
ye.voitsekhovska <sunakojane6@gmail.com>
2026-04-14 22:38:20 +0300

    client folder has been added

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import "@fortawesome/fontawesome-free/css/all.css";
+import Modal from "./components/Modal";
+import IdeaForm from "./components/ideaForm";
+import "./css/style.css";
+
+const modal = new Modal();
+const ideaForm = new IdeaForm();
+ideaForm.render();
