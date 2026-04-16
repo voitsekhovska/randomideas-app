b5bf88dc56df37705640d916e97b7fb7f4bb7b76
ye.voitsekhovska <sunakojane6@gmail.com>
2026-04-16 21:30:45 +0300

    added creating & getting ideas

--- client/src/components/IdeaForm.js
+++ client/src/components/IdeaForm.js
@@ -1,22 +1,30 @@
+import IdeasApi from "../services/ideasApi";
+import IdeaList from "./IdeaList";
+
 class IdeaForm {
   constructor() {
     this._formModal = document.querySelector("#form-modal");
+    this._ideaList = new IdeaList();
   }
 
   addEventListeners() {
     this._form.addEventListener("submit", this.handleSubmit.bind(this));
   }
 
-  handleSubmit(e) {
+  async handleSubmit(e) {
     e.preventDefault();
-    
-    const idea = {
-        text: this._form.elements.text.value,
-        tag: this._form.elements.tag.value,
-        username: this._form.elements.username.value
-    }
 
-    console.log(idea);
+    const idea = {
+      text: this._form.elements.text.value,
+      tag: this._form.elements.tag.value,
+      username: this._form.elements.username.value,
+    };
+
+    //Add idea to server
+    const newIdea = await IdeasApi.createIdea(idea);
+
+    //Add idea to list
+    this._ideaList.addIdeaToList(newIdea.data.data);
 
     // Clear fields
     this._form.elements.text.value = "";
