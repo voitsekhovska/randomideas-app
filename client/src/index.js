3f7bfc16be8f47a69f704329ff5b0362917ec76d
ye.voitsekhovska <sunakojane6@gmail.com>
2026-04-18 02:05:02 +0300

    changed to production mode

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import "@fortawesome/fontawesome-free/css/all.css";
+// import "@fortawesome/fontawesome-free/css/all.css";
 import Modal from "./components/Modal";
 import IdeaForm from "./components/ideaForm";
 import IdeaList from "./components/IdeaList";
